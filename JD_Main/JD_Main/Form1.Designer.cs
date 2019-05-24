887936fbde88afccd7a644059abd52c2c9502661
hupijekku <saarineneemelia@gmail.com>
2019-05-24 18:25:08 +0300

    Hope this works.. first time using git. + Importing Excel to datagridview

---
+++ JD_Main/JD_Main/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace JD_Main
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuTop = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tallennaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tuoExcel = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.menuTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuTop
+            // 
+            this.menuTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem});
+            this.menuTop.Location = new System.Drawing.Point(0, 0);
+            this.menuTop.Name = "menuTop";
+            this.menuTop.Size = new System.Drawing.Size(615, 24);
+            this.menuTop.TabIndex = 1;
+            this.menuTop.Text = "Menu";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tallennaToolStripMenuItem,
+            this.avaaToolStripMenuItem,
+            this.tuoExcel});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // tallennaToolStripMenuItem
+            // 
+            this.tallennaToolStripMenuItem.Name = "tallennaToolStripMenuItem";
+            this.tallennaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tallennaToolStripMenuItem.Text = "Tallenna";
+            // 
+            // avaaToolStripMenuItem
+            // 
+            this.avaaToolStripMenuItem.Name = "avaaToolStripMenuItem";
+            this.avaaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.avaaToolStripMenuItem.Text = "Avaa";
+            // 
+            // tuoExcel
+            // 
+            this.tuoExcel.Name = "tuoExcel";
+            this.tuoExcel.Size = new System.Drawing.Size(180, 22);
+            this.tuoExcel.Text = "Tuo Excelistä";
+            this.tuoExcel.Click += new System.EventHandler(this.tuoExcel_click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 27);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(591, 351);
+            this.dgv.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 390);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.menuTop);
+            this.MainMenuStrip = this.menuTop;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuTop.ResumeLayout(false);
+            this.menuTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuTop;
+        private System.Windows.Forms.ToolStripMenuItem tiedostoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tallennaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avaaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tuoExcel;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ JD_Main/JD_Main/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace JD_Main
+{
+    public partial class Form1 : Form
+    {
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkbook;
+        Excel.Worksheet xlWorksheet;
+        Excel.Range range;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tuoExcel_click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            int rowCount = 0;
+            int colCount = 0;
+
+            xlApp = new Excel.Application();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel file (*.xls*)|*.xls*";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+                xlWorkbook = xlApp.Workbooks.Open(
+                    filePath, 0, true, 5, "", "", true,
+                    Microsoft.Office.Interop.Excel.XlPlatform.xlWindows,
+                    "\t", false, false, 0, true, 1, 0
+                );
+
+                xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets.get_Item(1);
+
+                range = xlWorksheet.UsedRange;
+                rowCount = range.Rows.Count;
+                colCount = range.Columns.Count;
+
+                dgv.ColumnCount = colCount;
+
+                for (int i = 0; i < rowCount; i++)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(dgv);
+
+                    for (int j = 0; j < colCount; j++)
+                    {
+                        row.Cells[j].Value = (string)(range.Cells[i + 1, j + 1] as Excel.Range).Value2;
+                    }
+
+                    dgv.Rows.Add(row);
+                }
+
+
+                //string str = (string)(range.Cells[i, j] as Excel.Range).Value2;
+
+
+                xlWorkbook.Close();
+                xlApp.Quit();
+
+                Marshal.ReleaseComObject(xlWorksheet);
+                Marshal.ReleaseComObject(xlWorkbook);
+                Marshal.ReleaseComObject(xlApp);
+            }
+            
+        }
+    }
+}
